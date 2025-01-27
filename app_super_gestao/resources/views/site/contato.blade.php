c1e6b4ab0446652891c98aa92c3de96aa139482c
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2025-01-27 12:24:47 -0300

    Initial commit

---
+++ app_super_gestao/resources/views/site/contato.blade.php
@@ -0,0 +1,7 @@
+<h1>Contato (view)</h1>
+
+<ul>
+    <li><a href="/">Principal</a></li>
+    <li><a href="/contato">Contato</a></li>
+    <li><a href="/sobre-nos">Sobre-nós</a></li>
+</ul>
---
+++ app_super_gestao/resources/views/site/sobre-nos.blade.php
@@ -0,0 +1,7 @@
+<h1>Sobre Nós (view)</h1>
+
+<ul>
+    <li><a href="/">Principal</a></li>
+    <li><a href="/contato">Contato</a></li>
+    <li><a href="/sobre-nos">Sobre-nós</a></li>
+</ul>
