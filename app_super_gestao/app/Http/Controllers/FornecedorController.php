0947f9d76341f5d7f2823a6a4eeff5ab9f3b6607
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2025-01-29 14:26:02 -0300

    aula 51
    Operador condicional ternário

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -19,6 +19,9 @@ class FornecedorController extends Controller
             ]
         ];
 
+        // condição ? se verdade : se falso;
+        
+        echo isset($fornecedores[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ não informado';
 
         return view ('app.fornecedor.index', compact('fornecedores'));
     }
