b24ae1450702c5e9a4ad2c7063a198ae9390edce
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2025-01-29 12:10:52 -0300

    aula 47
    @if/else

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -3,10 +3,20 @@
 {{-- Aqui fica os comentarios descartados pelo interpretador do Blade --}}
 
 @php
-    // comentarios de linha unica no php
+    /*if() {
 
+    } elseif() {
 
-    /*
-        Comentarios de multiplas linhas no php 
-    */
-@endphp
+    } else {
+
+    }*/
+@endphp 
+{{--@dd($fornecedores)--}}
+
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Não há fornecedores cadastrados</h3>
+@endif
