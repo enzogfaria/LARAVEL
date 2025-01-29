873bf7828374b9cba1b520368f81b3b43e4c1fcb
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2025-01-29 14:09:56 -0300

    aula 50
    @empty

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -4,7 +4,18 @@
 
 @php
     /*
-    if(isset($variavel)) {} //retorna true se a variavel estiver definida
+    
+    if(empty($variavel)) {} //retorna true se a variavel estiver vazia
+    ex.:
+    - '' string vazia
+    - 0
+    - 0.0
+    - '0'
+    - null
+    - false
+    - array()
+    - $var variavel sem valor
+
     }*/
 @endphp 
 
@@ -13,4 +24,10 @@ Fornecedor: {{ $fornecedores[0]['nome'] }}
 <br>
 Status: {{ $fornecedores[0]['status'] }}
 <br>
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{$fornecedores[0]['cnpj'] }}
+        @empty($fornecedores[0]['cnpj'])
+            - Vazio
+        @endempty
+    @endisset
 @endisset
