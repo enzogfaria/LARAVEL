f9bedde0329eee51aae5681c5eb5bd64473ba5c6
Enzo Faria <123766918+enzogfaria@users.noreply.github.com>
2025-01-29 11:50:17 -0300

    aula 45
    comentarios e php puros no blade

---
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,12 @@
+<h1>Teste</h1>
+
+{{-- Aqui fica os comentarios descartados pelo interpretador do Blade --}}
+
+@php
+    // comentarios de linha unica no php
+
+
+    /*
+        Comentarios de multiplas linhas no php 
+    */
+@endphp
